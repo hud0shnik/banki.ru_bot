9ff590177de13e800ebde253f7a52ea4a9cda7de
Danila Egorov <c4nsero@mail.ru>
2021-11-23 17:35:03 +0300

    update main
    Изменил /help, теперь ответ более читаемый, также вернул ограничение, на всякий случай, проверки на неправильный ввод не делаю, потому что это бот для двух-трёх человек

--- main.go
+++ main.go
@@ -54,21 +54,20 @@ func getUpdates(botUrl string, offset int) ([]mods.Update, error) {
 
 //	https://core.telegram.org/bots/api#using-a-local-bot-api-server
 func respond(botUrl string, update mods.Update) error {
-
 	msg := update.Message.Text
 
 	if msg == "/help" || msg == "/start" {
-		mods.SendMsg(botUrl, update, "Чтобы подписать бота на тред, отправьте айди вашего сообщения и ссылку на сам тред в формате\n"+
-			"\"777 https://www.banki.ru/forum/?PAGE_NAME=read&FID=77&TID=77777&PAGEN_1=777#forum-message-list\"\n"+
+		mods.SendMsg(botUrl, update, "Чтобы подписать бота на тред, отправьте айди вашего сообщения и ссылку на сам тред в формате:\n"+
+			"\n\n777 https://www.banki.ru/forum/?PAGE_NAME=read&FID=77&TID=77777&PAGEN_1=777#forum-message-list\n\n"+
 			"когда появятся новые сообщения, бот вас оповестит\n")
 		return nil
 	}
 
-	if strings.Contains(msg, "https://www.banki.ru/forum/?PAGE_NAME=") {
+	if strings.Contains(msg, "https://www.banki.ru/forum/?PAGE_NAME=") && len(msg) > 63 {
 		go mods.Check(botUrl, update, msg)
 		return nil
 	}
 
-	mods.SendMsg(botUrl, update, "I don't understand, sorry")
+	mods.SendMsg(botUrl, update, "I don't understand")
 	return nil
 }
